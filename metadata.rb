e6080ab4bc5932e41d7c823e8c60252311d23e7d
Timomo <tyomo88@gmail.com>
2015-07-02 20:39:42 +0900

    rbenvのcookbookを切り出し

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'rbenv'
+maintainer       'timomo'
+maintainer_email 'tyomo88@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures rbenv'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: rbenv
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+group "rbenv" do
+  action :create
+end
+
+git "/opt/rbenv" do
+  repository "https://github.com/sstephenson/rbenv.git"
+  revision "master"
+  user "root"
+  group "rbenv"
+  action :sync
+end
+
+directory "/opt/rbenv/plugins" do
+  action :create
+end
+
+git "/opt/rbenv/plugins/ruby-build" do
+  repository "https://github.com/sstephenson/ruby-build.git"
+  revision "master"
+  user "root"
+  group "rbenv"
+  action :sync
+end
+
+template "/etc/profile.d/rbenv.sh" do
+  source "profile.d/rbenv.sh.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+# install ruby 2.1.6
+bash "install ruby" do
+  code <<-_EOH_
+    source /etc/profile.d/rbenv.sh
+    rbenv install 2.1.6
+    rbenv global 2.1.6
+  _EOH_
+  not_if { File.exist?("/opt/rbenv/shims/ruby") }
+end
